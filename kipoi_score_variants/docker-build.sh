5ac6dbc7a9e8ddfd619c384ed25ddb1bc536b757
Jason Chin <cschin@infoecho.net>
2018-06-19 10:36:32 -0700

    update docker configuration for running test with drone ci

--- kipoi_score_variants/docker-build.sh
+++ kipoi_score_variants/docker-build.sh
@@ -1,1 +1,3 @@
 docker build -t dx-kipoi-test dx-tk-docker/
+docker tag dx-kipoi-test:latest cschin/dx-kipoi-test:latest
+docker push cschin/dx-kipoi-test:latest
--- kipoi_score_variants/dx-tk-docker/run-vep-example.sh
+++ kipoi_score_variants/dx-tk-docker/run-vep-example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # echo $@
 source /dx-toolkit/dx-toolkit/environment; 
-dx login --token $1 --noprojects; 
+dx login --token ${DX_KIPOI_TEST_TOKEN} --noprojects; 
 dx select kipoi_model_zoo; 
 cd /dx-toolkit/kipoi_model_zoo_dx/kipoi_score_variants/test; 
-cat test-$2.sh | sed -e "s/--allow-ssh//" | bash
+cat test-${MODLE}.sh | sed -e "s/--allow-ssh//" | bash
